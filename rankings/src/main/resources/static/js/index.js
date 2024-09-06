57ae1257cf1668aa6e8890996d8d3701a36bebbf
xXEmanuXx <emanueledib@gmail.com>
2024-09-06 14:20:02 +0200

    working with 2024 data

--- rankings/src/main/resources/static/js/index.js
+++ rankings/src/main/resources/static/js/index.js
@@ -3,8 +3,8 @@
 const validateInput = (input) => {
     input.setCustomValidity('');
     const value = input.value.trim();
-    if (value.length != 10 || !value.startsWith("2023", 0) || isNaN(value)) {
-        input.setCustomValidity('Il numero di richiesta deve essere di 10 cifre e iniziare con 2023');
+    if (value.length != 10 || !value.startsWith("2024", 0) || isNaN(value)) {
+        input.setCustomValidity('Il numero di richiesta deve essere di 10 cifre e iniziare con 2024');
         return false;
     }
 
