4b395c2f9086026fc7c1df5d03ee1589a5c6a865
xXEmanuXx <emanueledib@gmail.com>
2026-09-10 11:48:35 +0200

    add: 2026 update (hopefully)

--- rankings/src/main/resources/static/js/index.js
+++ rankings/src/main/resources/static/js/index.js
@@ -3,8 +3,8 @@
 const validateInput = (input) => {
     input.setCustomValidity('');
     const value = input.value.trim();
-    if (value.length != 10 || !value.startsWith("2025", 0) || isNaN(value)) {
-        input.setCustomValidity('Il numero di richiesta deve essere di 10 cifre e iniziare con 2025');
+    if (value.length != 10 || !value.startsWith("2026", 0) || isNaN(value)) {
+        input.setCustomValidity('Il numero di richiesta deve essere di 10 cifre e iniziare con 2026');
         return false;
     }
 
